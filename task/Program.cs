1f72f89e1977bacce2b0cb0c9594cae71e11efcf
ALe329 <ale329@ya.ru>
2022-08-04 14:18:18 +0300

    create arrows

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+Clear();
+
+string[] arr1 = new string[5] {"78956", "Ho", "dog", "Hello", "pepsi"};
+string[] arr2 = new string[arr1.Length];
+
