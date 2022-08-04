361904f8c6934b12a56c7b8c066b6e37ee299350
ALe329 <ale329@ya.ru>
2022-08-04 14:21:20 +0300

    FillArray

--- task/Program.cs
+++ task/Program.cs
@@ -4,4 +4,19 @@ Clear();
 // Инициализация массивов
 string[] arr1 = new string[5] {"78956", "Ho", "dog", "Hello", "pepsi"};
 string[] arr2 = new string[arr1.Length];
+FillArray(arr1, arr2);
+PrintArray(arr2);
+// Метод заполнения второго массива строками из первого <= 3-х элементам
+void FillArray(string[] arr1, string[] arry2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
 
